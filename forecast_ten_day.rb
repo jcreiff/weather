2f04e3511e20e3945581ad4607f71adbd9cba3da
Joe Reiff <jcreiff@gmail.com>
2015-05-21 17:20:52 -0400

    create day method to access weekday abbreviations in 10dayF

--- forecast_ten_day.rb
+++ forecast_ten_day.rb
@@ -2,8 +2,14 @@ require 'httparty'
 
 class TenDayForecast
 
-  def initialize(zip)
+  attr_reader :page
 
+  def initialize(zip)
+    @page = HTTParty.get("http://api.wunderground.com/api/#{ENV["WUNDERGROUND_KEY"]}/forecast10day/q/#{zip}.json")
+  end
+
+  def day(num)
+    page["forecast"]["simpleforecast"]["forecastday"][num]["date"]["weekday_short"]
   end
 
 end
--- test.rb
+++ test.rb
@@ -3,7 +3,7 @@ require 'minitest/pride'
 require './weather_app.rb'
 
 class WeatherAppTest<Minitest::Test
-  
+
   def test_classes_exist
     assert ConditionsSummary.new(24210)
     assert TenDayForecast.new(24210)
@@ -11,6 +11,8 @@ class WeatherAppTest<Minitest::Test
     assert CurrentAlert.new(24210)
     assert ActiveHurricaneList.new(24210)
   end
+
+  # ConditionsSummary class tests
 
   def test_conditions_summary_knows_location
     a_town = ConditionsSummary.new(24210)
@@ -28,6 +30,14 @@ class WeatherAppTest<Minitest::Test
     refute_empty a_town.weather
     assert_equal "%", a_town.humidity[-1]
     assert_includes a_town.wind, "MPH"
+  end
+
+  # TenDayForecast class tests
+
+  def test_ten_day_returns_weekday_abbreviations
+    forecast = TenDayForecast.new(30322)
+    today = forecast.day(0)
+    assert_equal 3, today.length
   end
 
 end
