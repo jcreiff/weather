7890d96b952a00d3d86222a0b804d8a8b360e534
Joe Reiff <jcreiff@gmail.com>
2015-05-22 14:01:57 -0400

    create if/else to receive zip or city/state

--- weather_app.rb
+++ weather_app.rb
@@ -6,22 +6,35 @@ require './hurricanes.rb'
 
 
 print "Welcome to WeatheReport. Please enter your zipcode: "
-zip = gets.chomp
+input = gets.chomp
 
-current = ConditionsSummary.new(zip)
-ten_day = TenDayForecast.new(zip)
-sun_times = SunriseSunset.new(zip)
-alert = CurrentAlert.new(zip)
-canes = ActiveHurricaneList.new
+if input =~ /\d{5}/
+  zip = input
+elsif input =~/[a-zA-Z]+,\s[a-zA-Z]{2}\z/
+  array = input.split
+  array[-2] = array[-2][0..-2]
+  array[0..-2] = array[0..-2].map {|w| w.capitalize}
+  array[-1] = array[-1].upcase
+  city = array[0..-2].join("_")
+  state = array[-1]+"/"
+  name = state + city
+end
 
-
-current.display
-puts
-sun_times.display
-puts
-alert.display
-puts
-ten_day.display
-canes.display
-puts
-current.display_update
+#
+# current = ConditionsSummary.new(zip)
+# ten_day = TenDayForecast.new(zip)
+# sun_times = SunriseSunset.new(zip)
+# alert = CurrentAlert.new(zip)
+# canes = ActiveHurricaneList.new
+#
+#
+# current.display
+# puts
+# sun_times.display
+# puts
+# alert.display
+# puts
+# ten_day.display
+# canes.display
+# puts
+# current.display_update
